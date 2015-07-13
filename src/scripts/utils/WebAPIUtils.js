e625388656d320b81d04efcd949c8532782ca17f
Ian White <ian.white@t-3.com>
2015-07-13 08:43:34 -0500

    clean up comments and formatting

--- src/scripts/utils/WebAPIUtils.js
+++ src/scripts/utils/WebAPIUtils.js
@@ -9,26 +9,17 @@ const SideBarNavActions = require('../actions/SideBarNavActions'),
 module.exports = {
 
 	loadCategories() {
-		// wp.categories().get(function( err, data ) {
-	 //    if ( err ) {
-	 //        // handle err 
-	 //        console.log(err);
-	 //    }
-		//     // do something with the returned posts 
-		//     ServerActionCreators.receiveCategories({text: 'comment'});
-		// });
 
-		wp.categories().then(function( data ) {
-										    // do something with the returned posts 
-										     console.log(data);
-										    ServerActionCreators.receiveCategories(data);
-
-										}).catch(function( err ) {
-										    // handle error 
-										    console.log(err)
-										});
-										    // ServerActionCreators.receiveCategories([{text: 'comment'}]);
-	
+		wp.categories()
+			.then(function( data ) {
+		    // do something with the returned posts 
+		     console.log(data);
+		    ServerActionCreators.receiveCategories(data);
+			})
+			.catch(function( err ) {
+		    // handle error 
+		    console.log(err)
+			});
 	}
 
 };
