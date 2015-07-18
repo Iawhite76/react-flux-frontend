884f9cb87df05b473afcbb5e16bd8a19792bbc2d
Ian White <ian.white@t-3.com>
2015-07-17 19:55:06 -0500

    add Promise library and clean up WEBApiUtils

--- src/scripts/utils/WebAPIUtils.js
+++ src/scripts/utils/WebAPIUtils.js
@@ -1,67 +1,49 @@
 const SideBarNavActions = require('../actions/SideBarNavActionCreators'),
 			ServerActionCreators = require('../actions/ServerActionCreators'),
       Constants = require('../constants/Constants.js'),
-      request = require('superagent'),
+      Promise = require('promise'),
+      request = require('superagent-promise')(require('superagent'), Promise),
       APIEndpoints = Constants.APIEndpoints,
       WP = require( 'wordpress-rest-api' ),
       wp = new WP({ endpoint: APIEndpoints.WP_JSON }),
       buildMenu = require('./Utils').buildMenu;
       
-
-      function _getErrors(res) {
-        let errorMsgs = ["Something went wrong, please try again"],
-            json = JSON.parse(res.text);
-        if ((json)) {
-          if (json['errors']) {
-            errorMsgs = json['errors'];
-          } else if (json['error']) {
-            errorMsgs = [json['error']];
-          }
-        }
-        return errorMsgs;
-      }
-
 module.exports = {
 
 	loadNavigationMenu() {
 
-    request.get(APIEndpoints.WP_JSON + APIEndpoints.WP_MAIN_NAV_MENU)
-	  .set('Accept', 'application/json')
-	  .end(function(error, res) {
-	    if (res) {
-	      if (res.error) {
-	        let errorMsgs = _getErrors(res);
-	        // console.log(errorMsgs);
-	        // ServerActionCreators.receiveLogin(null, errorMsgs);
-	      } else {
-	        let json = JSON.parse(res.text);
-	        console.log(json);
-	        let navObj = buildMenu(json);
-	        ServerActionCreators.receiveNavigationMenuJSON(navObj);
-	      }
-	    }
-	  });
+		request('GET', APIEndpoints.WP_JSON + APIEndpoints.WP_MAIN_NAV_MENU)
+		  .set('Accept', 'application/json')
+		  .end()
+		  .then(function onResult(res) {
+		    // do stuff 
+		    let json = JSON.parse(res.text);
+		    console.log(json);
+		    let navObj = buildMenu(json);
+		    ServerActionCreators.receiveNavigationMenuJSON(navObj);
+		  })
+		  .catch(function(error) {
+		    // handle errors 
+		    console.log(error);
+		  });
 		
 	},
 
 	loadPages(query) {
-
-		request.get(APIEndpoints.QUERY_ROOT + query)
+		request('GET', APIEndpoints.QUERY_ROOT + query)
 		  .set('Accept', 'application/json')
-		  .end(function(error, res) {
-		    if (res) {
-		      if (res.error) {
-		        let errorMsgs = _getErrors(res);
-		        // console.log(errorMsgs);
-		        // ServerActionCreators.receiveLogin(null, errorMsgs);
-		      } else {
-		        // console.log(res.text);
-		        let json = JSON.parse(res.text);
-		        console.log(json);
-		        ServerActionCreators.receivePagesJsonArray(json);
-		      }
-		    }
+		  .end()
+		  .then(function onResult(res) {
+		    // do stuff 
+		    let json = JSON.parse(res.text);
+        console.log(json);
+        ServerActionCreators.receivePagesJsonArray(json);
+		  })
+		  .catch(function(error) {
+		    // handle errors 
+		    console.log(error);
 		  });
+
 	}
 
 };
