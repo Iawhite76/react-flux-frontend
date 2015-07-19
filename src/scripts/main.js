69c99a250037556e866fa59b35ea25af2c564cae
Ian White <ian.white@t-3.com>
2015-07-19 18:59:19 -0500

    add react router

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 require('bootstrap-webpack');
 require('../assets/stylesheets/style.styl');
+import { Router, Route } from 'react-router';
+import { history } from 'react-router/lib/BrowserHistory';
 
 // TODO: Require assets here.
 // require('../assets/images/product.png');
@@ -7,16 +9,26 @@ require('../assets/stylesheets/style.styl');
 const React = require('react'),
 			SideBarNav = require('./components/SideBarNav.jsx'),
 			Content = require('./components/Content');
+
 var App = React.createClass({
 
   render: function() {
     return (
       <div>
-        <SideBarNav />
-        <Content />
+       <SideBarNav />
+       <Content />
       </div>
     );
   }
 });
 
-React.render(<App />, document.getElementById('main'));
+React.render(<Router history={history}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>, document.getElementById('main'));
+
+// React.render(<Router history={new HashHistory}>
+//     <Route path="/" component={App}>
+//       <Route path="pages/:id" component={Page}/>
+//     </Route>
+//   </Router>, document.getElementById('main'));
